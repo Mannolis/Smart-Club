633ddcf3ab05b6b077c61490e1bef8c54a652464
Zeyad Lashean <zeyadlashean@gmail.com>
2025-11-29 21:11:56 +0200

    Added the list of clubs and socities to the Discover Clubs page

--- app/routes/discoverClubs.tsx
+++ app/routes/discoverClubs.tsx
@@ -3,9 +3,31 @@ import Navbar from "./navbar";
 
 export default function DiscoverClubs() {
   return (
-    <div>
+    <div className="clubs">
         <Navbar />
-        <h1>Discover Clubs</h1>
+        <h1>Current Clubs</h1>
+        <ul>
+          <li>Athletics club</li>
+          <li>Automotive Club</li>
+          <li>Chess Club</li>
+          <li>Consulting Club</li>
+          <li>Cooking club</li>
+          <li>Dance club</li>
+          <li>Finance and Investment Club</li>
+          <li>Math Club</li>
+          <li>MUN Club</li>
+          <li>Music club</li>
+          <li>Pause the Loop Club</li>
+          <li>Personal Branding/Digital Marketing Club</li>
+          <li>Philosophy Club</li>
+          <li>Programming club</li>
+          <li>Robotics Club</li>
+          <li>Running Club</li>
+          <li>Social Events Club</li>
+          <li>Stargazing club</li>
+          <li>Tennis and Paddle Club</li>
+          <li>UMOJA Club</li>
+        </ul>
     </div>
   );
 }
