0752765f3feef7d34bd9ab01890f37ccfa13a1e1
Zeyad Lashean <zeyadlashean@gmail.com>
2025-12-02 01:56:31 +0200

    Made the styles better, added the signup buttons, and fixed the json files

--- app/routes/discover-clubs.$clubId.tsx
+++ app/routes/discover-clubs.$clubId.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./navbar";
 import { useParams } from "react-router";
 import clubsData from "../data/clubs.json"
+import { SignupButton } from "./home";
 
 export default function clubPages() {
     const { clubId } = useParams(); //The ID of the club I want
@@ -19,16 +20,29 @@ export default function clubPages() {
         <div>
             <Navbar />
             <section className = "for-you">
-            <h1>{club.name}</h1>
-            <p>{club.description}</p>
-            <h2>Upcoming Events</h2>
-                {club.events.map((event) =>(
-                    <div key = {event.id} className = "event-card">
-                        <h3 className = "event-title">{event.title}</h3>
-                        <p className = "event-description">{event.description}</p>
-                        <p className = "event-time">{event.date}</p>
+                <h1>{club.name}</h1>
+                <p style={{textAlign: "center", fontFamily: "Arial, sans-serif", fontSize: "16px"}}>{club.description}</p>
+                {club.events.length === 0 ? (
+                    <h2>No events yet.</h2>
+                ) : (
+                    <>
+                    <h2> Upcoming Events</h2>
+                    <div className = "events-grid">
+                    {club.events.map((event) =>(
+                        <div key = {event.id} className = "event-card">
+                            <h3 className = "event-title">{event.title}</h3>
+                            <p className = "event-description">{event.description}</p>
+                            <p className = "event-time">{event.date}</p>
+                            <div className="event-buttons">
+                                <SignupButton />
+                                <button className="contact-btn">Learn More</button>
+                            </div>
+                        </div>
+                    ))}
                     </div>
-                ))}
+                    </>
+                )
+                }
             </section>
         </div>
     )
